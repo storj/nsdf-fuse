2c721c115344102a34543cbd97de681406d05d11
Bill Thorp <billt@storj.io>
2023-06-30 14:30:39 -0400

    minor run-all fixes

--- run-all.sh
+++ run-all.sh
@@ -2,10 +2,15 @@ aws configure set default.s3.max_concurrent_requests 10
 
 for target in goofys geesefs rclone s3backer s3fs s3ql # objectivefs juicefs
 do
-    echo $value
+    echo *******************************
+    echo * $target
+    echo *******************************
+
     export TARGET=$target
     export OUTPUT_FILE=$target.txt
-    nsdf-install
-    nsdf-fuse clean-all && nsdf-fuse create-bucket && nsdf-fuse simple-benchmark
+    ./nsdf-fuse install
+    ./nsdf-fuse clean-all
+    ./nsdf-fuse create-bucket
+    ./nsdf-fuse simple-benchmark
 done
 
