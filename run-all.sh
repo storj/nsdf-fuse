01b90fddfdbe2beffc12bf3fbf2c5d577c4f9227
Bill Thorp <billt@storj.io>
2023-06-30 14:37:37 -0400

    check run-all env

--- run-all.sh
+++ run-all.sh
@@ -1,4 +1,12 @@
 aws configure set default.s3.max_concurrent_requests 10
+if [[ -z "${AWS_ACCESS_KEY_ID}" ]]; then
+  exit 1
+fi
+if [[ -z "${AWS_SECRET_ACCESS_KEY}" ]]; then
+  exit 1
+fi
+export AWS_DEFAULT_REGION_REGION=us-east-1
+export AWS_S3_ENDPOINT_URL=https://gateway.storjshare.io
 
 for target in goofys geesefs rclone s3backer s3fs s3ql # objectivefs juicefs
 do
